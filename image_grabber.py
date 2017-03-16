b713a64f62e19051d44db1a3141b22a5003aeb50
Anthony <@home>
2017-03-16 16:54:46 -0500

    Fixed image_grabber

--- image_grabber.py
+++ image_grabber.py
@@ -5,7 +5,7 @@ import os
 file = open('states.json').read()
 data = json.loads(file)
 output_dir = "img"
-BASE_PATH = "https://www.usming.gov/images/mint_programs/50sq_program/states/"
+BASE_PATH = "https://www.usmint.gov/images/mint_programs/50sq_program/states/"
 
 # XX_Designs.gif
 
@@ -15,6 +15,3 @@ for key in data:
     print(url)
     output_file = os.path.join(os.path.curdir, output_dir, filename)
     urllib.request.urlretrieve(url, output_file)
-
-
-
