4a867866f637d33ac9450d3e8131d4fdfa68230e
Larry Lade <lplade@users.noreply.github.com>
2017-03-16 15:21:01 -0500

    Updated model for manual primary key

--- quarter_collector/models.py
+++ quarter_collector/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Quarter(models.Model):
+    state_code = models.CharField(max_length=2, primary_key=True)
     state = models.CharField(max_length=14)
     year_issued = models.IntegerField()
     description = models.TextField(null=True, blank=True)
