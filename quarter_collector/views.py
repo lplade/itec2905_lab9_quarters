30bd267caeaca32e8f2440f7e38e62d42ca1a68d
Larry Lade <lplade@users.noreply.github.com>
2017-03-17 00:34:02 -0500

    reorganized views

--- quarter_collector/views.py
+++ quarter_collector/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf.urls import url
 from .models import Quarter
 
+
 # def quarter_list
 def index(request):
-    return render(request, 'quarter_collector\index.html', {})
+    return render(request, 'index.html', {})
+
+
 # def quarter_detail
 def quarter_detail(request):
-    return render(request, 'quarter_collector\test.html')
+    return render(request, 'test.html')
